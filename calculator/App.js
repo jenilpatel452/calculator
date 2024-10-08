703b1b522acee65dd1aadaad2200de59191e2500
priya2011k <kakadiyapriya8@gmail.com>
2024-10-08 08:56:37 +0530

    cal

---
+++ calculator/App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Razorpay from './Razorpay'
+
+const App = () => {
+  return (
+    <View>
+     <Razorpay/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
